43c7a8ab4785c4f434bd16c977461259464e648e
Ramiro Vazquez <ramirovazquez@gmail.com>
2022-06-21 09:04:16 -0300

    init

---
+++ index.js
@@ -0,0 +1,15 @@
+const { CowSdk } = require('@cowprotocol/cow-sdk')
+
+const chainId = 4 // Rinkeby
+const cowSdk = new CowSdk(chainId)
+
+async function run() {
+    const trades = await cowSdk.cowApi.getOrders({
+        owner: '0x1811be0994930fE9480eAEDe25165608B093ad7A', // Trader
+        limit: 5,
+        offset: 0,
+    })
+    console.log(trades)
+}
+
+run()
